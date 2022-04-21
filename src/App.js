d95383e010965fc0e3f46265a945eb8d6de49aed
erynder-z <stfn.bgr@gmail.com>
2022-04-21 20:38:19 +0200

    Add nested routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-const App = () => {
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="coolstuff" element={<Coolstuff />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Layout() {
   return (
     <div>
-      <h1>Hello from App</h1>
-      <ul>
-        <li>
-          <a href="/profile">goto Profile</a>
-        </li>
-        <li>
-          <a href="/home">goto Home</a>
-        </li>
-        <li>
-          <a href="/about">goto About</a>
-        </li>
-      </ul>
+      <h1>Welcome to the app!</h1>
+      <nav>
+        <Link to="coolstuff">Cool Stuff</Link> |{' '}
+        <Link to="dashboard">Dashboard</Link>
+      </nav>
+      <div className="content">
+        <Outlet />
+      </div>
     </div>
   );
-};
+}
+
+function Coolstuff() {
+  return <h1>Cool Stuff</h1>;
+}
+
+function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
 
 export default App;
